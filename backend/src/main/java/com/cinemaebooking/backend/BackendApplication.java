d397f9ad2d5745741b7eb5acef63e1d29e367b81
AdriGodaCS <acg44605@uga.edu>
2026-09-01 13:29:29 -0400

    pull mongodb uri from .env

--- backend/src/main/java/com/cinemaebooking/backend/BackendApplication.java
+++ backend/src/main/java/com/cinemaebooking/backend/BackendApplication.java
@@ -3,10 +3,19 @@ package com.cinemaebooking.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		 // Load .env and copy to System properties so Spring can see them
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+		
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
