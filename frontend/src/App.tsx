e806466a71677cad4adfc9b80d780e9bb940730d
jplex771 <jpdlr2005@gmail.com>
2026-09-18 19:32:25 -0400

    MovieDetails wired

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import RootLayout from './layouts/RootLayout'
+import { MovieDetails } from './pages/MovieDetails'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<RootLayout />}>
-          <Route path="/" element={<Home />} />          
+          <Route path="/" element={<Home />} />
+          <Route path="/movie{id}" element={<MovieDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
